22e5345c28b4c0108c42fa07868e1ab72f666533
myasir1994 <xtrangeryasir@gmail.com>
2017-09-15 16:00:35 +0500

    hello world to myworld

--- app/src/main/java/com/testproject/MainActivity.java
+++ app/src/main/java/com/testproject/MainActivity.java
@@ -2,12 +2,18 @@ package com.testproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt = (TextView) findViewById(R.id.txt);
+
+        txt.setText("My World");
     }
 }
